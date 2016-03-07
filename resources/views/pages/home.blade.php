fa306255a7e29bc9ebe06fb265b670081f8ffc61
System Administrator <root@mnj153dty3.rackspace.corp>
2016-03-07 14:41:58 -0600

    Corrected duplicate login... now we are getting somewhere!

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,19 +5,5 @@
 
       @include('layouts.partials.login')
 
-      <form class="form-signin">
-        <h2 class="form-signin-heading">Welcome to NotQuiteReddit!</h2>
-        <label for="inputEmail" class="sr-only">Username</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password" id="inputPassword" class="form-control" placeholder="Super Secret Password" required>
-        <div class="checkbox">
-          <label>
-            <input type="checkbox" value="remember-me"> Never give me up. Never let me down. Never run around and desert me.
-          </label>
-        </div>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign In Now!</button>
-      </form>
-
     </div> 
 
