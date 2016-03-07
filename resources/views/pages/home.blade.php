6ae482707fa998006254889fcd20fb161523391f
System Administrator <root@mnj153dty3.rackspace.corp>
2016-03-07 13:09:50 -0600

    Adding some mods to the home.blade.php

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -27,7 +27,7 @@
       <form class="form-signin">
         <h2 class="form-signin-heading">Welcome to NotQuiteReddit!</h2>
         <label for="inputEmail" class="sr-only">Username</label>
-        <input type="email" id="inputEmail" class="form-control" placeholder="USERNAME" required autofocus>
+        <input type="email" id="inputEmail" class="form-control" placeholder="Username" required autofocus>
         <label for="inputPassword" class="sr-only">Password</label>
         <input type="password" id="inputPassword" class="form-control" placeholder="Super Secret Password" required>
         <div class="checkbox">
