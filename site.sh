8d3968133e2d86e54b12acb857736a861ee63b5d
System Administrator <root@mnj153dty3.rackspace.corp>
2016-03-07 09:52:34 -0600

    Changed name to NotQuiteReddit.git in repo

--- site.sh
+++ site.sh
@@ -1,6 +1,6 @@
 ## Server Site Automated Deployment Script
-rm -rf shittyReddit
-git clone https://github.com/caucajun/shittyReddit.git
-cp -af shittyReddit/* /var/www/html/shittyReddit/forum
+rm -rf NotQuiteReddit
+git clone https://github.com/caucajun/NotQuiteReddit.git
+cp -af NotQuiteReddit/* /var/www/html/shittyReddit/forum
 chmod -R o+w /var/www/html/shittyReddit/forum/storage
 exit
