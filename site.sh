49dc8d750e5f10caf526277059b5eb2cb9911051
root <root@server-01.(none)>
2016-03-06 22:30:20 -0600

    Fixed site.sh permissions on ./storage

--- site.sh
+++ site.sh
@@ -2,4 +2,5 @@
 rm -rf shittyReddit
 git clone https://github.com/caucajun/shittyReddit.git
 cp -af shittyReddit/* /var/www/html/shittyReddit/forum
+chmod -R o+w /var/www/html/shittyReddit/forum/storage
 exit
